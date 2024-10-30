3db6a5667261b14d5f7c5caf69aacf48eefb2e82
Rayan581 <ra3328600@gmail.com>
2024-10-30 18:52:11 +0500

    -Fixed insertion logic

--- AI_B_A3_i230018.cpp
+++ AI_B_A3_i230018.cpp
@@ -14,22 +14,22 @@ public:
 
     Game() {}
 
-    Game(string gameId, string name, string developer, string publisher, float file_size_in_GB, int downloads)
+    Game(string _gameId, string _name, string _developer, string _publisher, float _file_size_in_GB, int _downloads)
     {
-        this->gameId = gameId;
-        this->name = name;
-        this->developer = developer;
-        this->publisher = publisher;
-        this->file_size_in_GB = file_size_in_GB;
-        this->downloads = downloads;
+        gameId = _gameId;
+        name = _name;
+        developer = _developer;
+        publisher = _publisher;
+        file_size_in_GB = _file_size_in_GB;
+        downloads = _downloads;
     }
 };
 
 class Game_Played
 {
-private:
-    struct Node
+    class Node
     {
+    public:
         Game game;
         float hours_played;
         int achievements_unlocked;
@@ -37,92 +37,57 @@ private:
         int height;
         Node *right;
         Node *left;
+
+        Node()
+        {
+            right = NULL;
+            left = NULL;
+        }
     };
     Node *root;
 
-    /**
-     * @brief Performs a right rotation on a node in the binary search tree.
-     * This function is used to balance the tree after an insertion or deletion operation.
-     * @param node The node to perform the rotation on.
-     * @return The new root node after the rotation.
-     */
-    Node *rightRotate(Node *node)
+    // Returns if the number stored in string form is greater than the other
+    bool isGreater(string str1, string str2)
     {
-        Node *new_root = node->left;
-        node->left = new_root->right;
-        new_root->right = node;
-        return new_root;
+        int len1 = 0;
+        int len2 = 0;
+
+        for (int i = 0; str1[i] != '\0'; i++)
+            len1++;
+        for (int i = 0; str2[i] != '\0'; i++)
+            len2++;
+
+        if (len1 > len2)
+            return true;
+        else if (len1 < len2)
+            return false;
+
+        for (int i = 0; i < len1; i++)
+            if (str1[i] > str2[i])
+                return true;
+            else if (str1[i] < str2[i])
+                return false;
+
+        return false;
     }
 
-    /**
-     * @brief Performs a left rotation on a node in the binary search tree.
-     * This function is used to balance the tree after an insertion or deletion operation.
-     * @param node The node to perform the rotation on.
-     * @return The new root node after the rotation.
-     */
-    Node *leftRotate(Node *node)
+    Node *insert(Node *node, Game game, float hours_played, int achievements_unlocked)
     {
-        Node *new_root = node->right;
-        node->right = new_root->left;
-        new_root->left = node;
-        return new_root;
-    }
-
-    /**
-     * @brief Performs a left-right rotation on a node in the binary search tree.
-     * This function is used to balance the tree after an insertion or deletion operation.
-     * @param node The node to perform the rotation on.
-     * @return The new root node after the rotation.
-     */
-    Node *leftRightRotate(Node *node)
-    {
-        node->left = leftRotate(node->left);
-        return rightRotate(node);
-    }
-
-    /**
-     * @brief Performs a right-left rotation on a node in the binary search tree.
-     * This function is used to balance the tree after an insertion or deletion operation.
-     * @param node The node to perform the rotation on.
-     * @return The new root node after the rotation.
-     */
-    Node *rightLeftRotate(Node *node)
-    {
-        node->right = rightRotate(node->right);
-        return leftRotate(node);
-    }
-
-    Node *insert(Node *R, Game game, float hours_played, int achievements_unlocked)
-    {
-        if (R == NULL)
+        if (node == NULL)
         {
-            // Create a new node and return it as the new root of this subtree
             Node *newNode = new Node;
             newNode->game = game;
             newNode->hours_played = hours_played;
             newNode->achievements_unlocked = achievements_unlocked;
-            newNode->height = 0;
+            newNode->height = 1;
             return newNode;
         }
+        if (isGreater(node->game.gameId, game.gameId))
+            node->left = insert(node->left, game, hours_played, achievements_unlocked);
+        else if (isGreater(game.gameId, node->game.gameId))
+            node->right = insert(node->right, game, hours_played, achievements_unlocked);
 
-        // Insert based on gameId ordering
-        if (game.gameId < R->game.gameId)
-        {
-            R->left = insert(R->left, game, hours_played, achievements_unlocked);
-        }
-        else if (game.gameId > R->game.gameId)
-        {
-            R->right = insert(R->right, game, hours_played, achievements_unlocked);
-        }
-        else
-        {
-            // Duplicate gameId; handle as needed, e.g., return without insertion
-            return R;
-        }
-
-        // Update height of the node
-        R->height = 1 + max(Height(R->left), Height(R->right));
-        return R;
+        return node;
     }
 
 public:
@@ -131,136 +96,23 @@ public:
         root = NULL;
     }
 
-    /**
-     * @brief Returns the height of a node in the binary search tree.
-     *
-     * This function returns the height of a given node in the binary search tree.
-     * If the node is NULL, it returns -1.
-     *
-     * @param node The node for which the height is to be calculated.
-     *
-     * @return The height of the node.
-     */
-    int Height(Node *node)
-    {
-        if (node == NULL)
-            return -1;
-        else
-            return node->height;
-    }
-
-    /**
-     * @brief Returns the root node of the binary search tree.
-     *
-     * This function returns the root node of the binary search tree.
-     * @return The root node of the binary search tree.
-     */
     Node *getRoot()
     {
         return root;
     }
 
-    /**
-     * @brief Returns the balance factor of a node in the binary search tree.
-     *
-     * This function returns the balance factor of a given node in the binary search tree.
-     *
-     * @param node The node for which the balance factor is to be calculated.
-     * @return The balance factor of the node.
-     */
-    int BalanceFactor(Node *node)
-    {
-        return Height(node->left) - Height(node->right);
-    }
-
-    /**
-     * @brief Inserts a new game into the binary search tree.
-     *
-     * This function creates a new node with the given game information and inserts
-     * it into the binary search tree based on the game's ID.
-     *
-     * @param game The Game object to be inserted.
-     * @param hours_played The number of hours played for this game.
-     * @param achievements_unlocked The number of achievements unlocked for this game.
-     *
-     * @return void
-     */
     void insert(Game game, float hours_played, int achievements_unlocked)
     {
         root = insert(root, game, hours_played, achievements_unlocked);
     }
 
-    void print(Node *R)
+    void print(Node *node)
     {
-        if (R == NULL)
+        if (node == NULL)
             return;
-        print(R->left);
-        cout << "Game ID: " << R->game.gameId << endl;
-        cout << "Name: " << R->game.name << endl;
-        cout << "Developer: " << R->game.developer << endl;
-        cout << "Publisher: " << R->game.publisher << endl;
-        cout << "File Size: " << R->game.file_size_in_GB << " GB" << endl;
-        cout << "Downloads: " << R->game.downloads << endl;
-        cout << "Hours Played: " << R->hours_played << endl;
-        cout << "Achievements Unlocked: " << R->achievements_unlocked << endl;
-        cout << endl;
-        print(R->right);
-    }
-};
-
-class Player
-{
-    struct Node
-    {
-        string playerId;
-        string name;
-        string phoneNo;
-        string email;
-        string password;
-        Game_Played games_played;
-
-        Node *right;
-        Node *left;
-    };
-    Node *root;
-
-public:
-    Player()
-    {
-        root = NULL;
-    }
-
-    /**
-     * @brief Returns the root node of the binary search tree.
-     *
-     * This function returns the root node of the binary search tree.
-     * @return The root node of the binary search tree.
-     */
-    Node *getRoot()
-    {
-        return root;
-    }
-
-    // Insert a game in a player's list of games played
-    Node *insert_game(Game game, float hours_played, int achievements_unlocked, string playerId, Node *R)
-    {
-        if (R == NULL)
-        {
-            R = new Node;
-            R->playerId = playerId;
-            R->games_played.insert(game, hours_played, achievements_unlocked);
-        }
-        return R;
-    }
-
-    void print()
-    {
-        Node *temp = root;
-        while (temp != NULL)
-        {
-            cout << temp->playerId << endl;
-            temp = temp->right;
-        }
+        print(node->left);
+        cout << node->game.gameId << " " << node->game.name << " " << node->game.developer << " " << node->game.publisher << " " << node->game.file_size_in_GB << " " << node->game.downloads << " " << node->hours_played << " " << node->achievements_unlocked << endl;
+        print(node->right);
     }
 };
 
@@ -268,10 +120,11 @@ int main()
 {
     Game_Played game;
 
-    game.insert(Game("1", "Game1", "Developer1", "Publisher1", 1.5, 1000), 10.5, 5);
-    game.insert(Game("2", "Game2", "Developer2", "Publisher2", 2.0, 2000), 20.0, 10);
-    game.insert(Game("3", "Game3", "Developer3", "Publisher3", 1.0, 1500), 15.0, 7);
-    game.insert(Game("4", "Game4", "Developer4", "Publisher4", 1.5, 1200), 12.0, 4);
+    game.insert(Game("1", "a", "a", "a", 1, 1), 1, 1);
+    game.insert(Game("2", "b", "b", "b", 2, 2), 2, 2);
+    game.insert(Game("3", "c", "c", "c", 3, 3), 3, 3);
+    game.insert(Game("10", "d", "d", "d", 4, 4), 4, 4);
+    game.insert(Game("5", "e", "e", "e", 5, 5), 5, 5);
 
     game.print(game.getRoot());
 
